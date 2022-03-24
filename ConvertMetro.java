ed059c8e838903e009f371fd83512094082418bb
mayckie <grdsza@gmail.com>
2022-03-23 22:25:16 -0300

    First commit

---
+++ ConvertMetro.java
@@ -0,0 +1,21 @@
+
+package convertmetro;
+
+import java.util.Scanner;
+
+
+public class ConvertMetro {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner tec = new Scanner(System.in);
+        int metro = 100;
+        System.out.println("Digite uma metragem: ");
+        int resp = tec.nextInt();
+        int resu = resp * metro;
+        System.out.println("Transformamos " + resp + " metro(s) em centimetros e o resuultado é: " + resu + " centimetros!");
+          
+    }
+    
+}
